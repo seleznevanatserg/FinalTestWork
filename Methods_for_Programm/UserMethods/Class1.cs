880914f8ecd944b7a7e1ad7f4cdd9bb9f170fdb4
SeleznevAS <seleznevanatserg@yahoo.com>
2022-12-30 17:44:40 +0300

    Progrmm edited and Methods_for_Programm created

---
+++ Methods_for_Programm/UserMethods/Class1.cs
@@ -0,0 +1,64 @@
+﻿namespace UserMethods;
+
+public class Method
+{
+
+    // Метод считывания из строки ввода и преобразования число.
+    public static int InputParsedIntNumber()
+    {
+        bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+        if (!isParsed) return 0;
+        else return num;
+    }
+
+ 
+
+    //Метод проверки корректного ввода и исключения 0.
+    public static int ChekingNumberForPositive(int number, string msgOfError = "")
+    {
+        if (number < 1 || number > 9999999)
+        {
+            Console.WriteLine("Uncorrect number");
+            while (number < 1 || number > 9999999)
+            {
+                Console.WriteLine(msgOfError);
+                number = InputParsedIntNumber();
+            }
+        }
+        return number;
+    }
+
+
+
+    // Метод для формирования исходного однострочного массива строк.
+    public static string[] GenerateRandomWords(int num)
+    {
+        Random random = new Random();
+        string[] array = new string[num];
+        for (int i = 0; i < array.Length; i++)
+        {
+            int rnd = random.Next(1, 7);
+            array[i] = RandomWord(rnd);
+        }
+        return array;
+    }
+
+    // Метод печати одномерного массива.
+    public static void PrintArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++) Console.Write($"[{i + 1}]{array[i]}  ");
+        Console.WriteLine();
+    }
+
+    // Метод генерации слова (набора символов) путём выбора случайных элементов строки.
+    public static string RandomWord(int rnd, string rndWord = "")
+    {
+        Random random = new Random();
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        if (rnd > 0) rndWord = rndWord + alphabet[random.Next(0, alphabet.Length)] + RandomWord(rnd - 1);
+        return rndWord;
+    }
+
+
+
+}
--- Programm_for_Step_4/Program.cs
+++ Programm_for_Step_4/Program.cs
@@ -1,11 +1,12 @@
 ﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 длина которых меньше или равна 3 символа. */
 
+using static UserMethods.Method;
 Random random = new Random();
-
-MessageForAmountWords();
+string msgAmountWords = "Enter number for amount generate words, please. It's number > 0.";
+Console.WriteLine(msgAmountWords);
 int amountWords = InputParsedIntNumber();
-amountWords = ChekingNumberForPositive(amountWords);
+amountWords = ChekingNumberForPositive(amountWords, msgAmountWords);
 int count = 0;
 string[] wordArray = GenerateRandomWords(amountWords);
 Console.WriteLine("The following words were created: [serial_number]<word>");
@@ -43,55 +44,6 @@ string[] SelectedWordsShort3Symbols(string[] array, out int count)
     return newArray;
 }
 
-// Метод печати одномерного массива.
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++) Console.Write($"[{i + 1}]{array[i]}  ");
-    Console.WriteLine();
-}
-
-// Метод для формирования исходного однострочного массива строк.
-string[] GenerateRandomWords(int num)
-{
-    string[] array = new string[num];
-    for (int i = 0; i < array.Length; i++)
-    {
-        int rnd = random.Next(1, 7);
-        array[i] = RandomWord(rnd);
-    }
-    return array;
-}
-
-// Метод генерации слова (набора символов) путём выбора случайных элементов строки.
-string RandomWord(int rnd, string rndWord = "")
-{
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    if (rnd > 0) rndWord = rndWord + alphabet[random.Next(0, alphabet.Length)] + RandomWord(rnd - 1);
-    return rndWord;
-}
-
-// Метод считывания из строки ввода и преобразования число.
-int InputParsedIntNumber()
-{
-    bool isParsed = int.TryParse(Console.ReadLine(), out int num);
-    if (!isParsed) return 0;
-    else return num;
-}
-
-//Метод проверки корректного ввода и исключения 0.
-int ChekingNumberForPositive(int number)
-{
-    if (number < 1 || number > 9999999)
-    {
-        Console.WriteLine("Uncorrect number");
-        while (number < 1 || number > 9999999)
-        {
-            MessageForAmountWords();
-            number = InputParsedIntNumber();
-        }
-    }
-    return number;
-}
 
 // Методы сообщений 
 // Сообщение о количестве обнаруженных строк длиной 3 символа и меньше.
@@ -106,9 +58,4 @@ void MessageForZeroArray()
     string msgZeroArr = "Sorry, but this array don't have words long 3 symbols and less.";
     Console.WriteLine(msgZeroArr);
 }
-// Сообщение с предложением ввода количества генерируемых слов (наборов символов).
-void MessageForAmountWords()
-{
-    string msgAmountWords = "Enter number for amount generate words, please. It's number > 0.";
-    Console.WriteLine(msgAmountWords);
-}
+
